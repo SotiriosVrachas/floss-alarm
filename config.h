ff11785aee76b9befc1ef630d45ed996d358d54e
Sotirios Vrachas <sotirios@vrachas.com>
2016-10-08 05:10:24 +0300

    abstration

--- config.h
+++ config.h
@@ -7,3 +7,23 @@
 
 #define exit_delay 10000 //Time to arm and leave
 #define entry_delay 10000 //Time to enter and disarm
+
+#define zones_size 6  //number of zones
+
+// in this matrix configure the zones
+// pin, mode, safevalue, delay1, delay2, armedonly, admin
+static int zones[zones_size][7] = {
+  {4, INPUT_PULLUP, 0, 0, 0, 0, 0},
+  {6, INPUT, 1, 0, 0, 0, 1},
+  {3, INPUT, 1, 0, 0, 0, 0},
+  {2, OUTPUT, 0, 0, 0, 0, 0},
+  {1, INPUT, 0, 10000, 10000, 1, 0}
+};
+
+static int char zone_names[zones_size][0] = {
+  {"fire but"},
+  {"tamper zone"},
+  {"fire zone"},
+  {"siren"},
+  {"perimeter"}
+};
--- floss-alarm.ino
+++ floss-alarm.ino
@@ -1,8 +1,8 @@
-/*  
+/*
     floss-alarm.ino - hsgr digispark based alarm prototype
-    
+
     Copyright (C) 2016 Sotirios Vrachas <sotirios@vrachas.com>
-    
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -15,59 +15,83 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+*/
 
 #include <OneWire.h>
 #include "keys.h"
 #include "config.h"
 
 boolean g_armed;
+unsigned long flashTimeMark = 0;
+
+//status, time
+long unsigned int zones_stat[zones_size][2];
 
 OneWire ds(onewire);  // OneWire read pin
 
-void setup(){
-  pinMode(intrudersensors, INPUT);  //Perimeter Sensors
-  pinMode(sirens, OUTPUT); //Alarm
-  pinMode(firesensors, INPUT);  //Fire Sensors
-  pinMode(firebutton, INPUT_PULLUP);  //Fire Alarm Suspend/Fire Test
-  pinMode(tamper, INPUT);  //Fire Alarm Suspend/Fire Test
+void setup() {
+  for (int z = 0; z < zones_size; z++) {
+    pinMode(zones[z][0], zones[z][1]);
+  }
 }
- 
-void loop(){
-  if(check_arm()){
-    delay(exit_delay); //Time to arm and leave
-    while(check_arm()){
-      if (detect_intruder()){
-        delay(entry_delay); //Time to enter and disarm
-        if(check_arm()){
-          alert(intrudersensors);
+
+void loop() {
+  alarm();
+}
+
+
+boolean is_safe(int z) {
+  switch ( zones[z][1] ) {
+    case INPUT:
+      return (digitalRead(firesensors) == zones[z][2]);
+    case INPUT_PULLUP:
+      return (digitalRead(firesensors) == zones[z][2]);
+    case OUTPUT:
+      return true;
+  }
+}
+
+void alarm() {
+  for (int z = 0; z < zones_size; z++) {
+    if ((!is_safe(z) || zones_stat[z][0]) && IsTime(&zones_stat[z][1], zones[z][3]) && (!zones[z][5] || (zones[z][5] && check_arm())) && (!zones[z][6] || (zones[z][5] && isAdmin()))) {
+      alert(zone_names[z]);
+    }
+  }
+  if (check_arm()) {
+    if (IsTime(&flashTimeMark, exit_delay)) {
+      while (check_arm()) {
+        if (detect_intruder()) {
+          delay(entry_delay); //Time to enter and disarm
+          if (check_arm()) {
+            alert(intrudersensors);
+          }
+        } else if (detect_fire()) {
+          alert(firesensors);
         }
-      } else if(detect_fire()){
-        alert(firesensors);
       }
     }
   } else {
-    if(detect_fire()){
+    if (detect_fire()) {
       fire_alarm();
     }
   }
 }
- 
-void alert(int zone){
+
+void alert(int zone) {
   digitalWrite(sirens, HIGH);
-  
+
   //Send SMS to operators including zone.
-  //TODO: Add function with AT Comands loop here 
-  
-  while(check_arm()){
+  //TODO: Add function with AT Comands loop here
+
+  while (check_arm()) {
     delay(100);
   }
   digitalWrite(sirens, LOW);
 }
 
-void fire_alarm(){
+void fire_alarm() {
   digitalWrite(sirens, HIGH);
-  while(check_fire_button()){
+  while (check_fire_button()) {
     delay(100);
   }
   digitalWrite(sirens, LOW);
@@ -77,10 +101,10 @@ boolean detect_intruder() {
   return (digitalRead(intrudersensors) == LOW);
 }
 
-boolean check_arm(){
+boolean check_arm() {
   int permited = 0;
   byte id[8] = { 0, 0, 0, 0, 0, 0, 0, 0};
-  
+
   if ( !ds.search(id)) {
     ds.reset_search();
     delay(100);
@@ -88,35 +112,50 @@ boolean check_arm(){
   }
 
   //Go through keys permitted for an ID match
-  for(int k = 0; k < BD_size; k++) {
-    permited=0;
+  for (int k = 0; k < BD_size; k++) {
+    permited = 0;
 
     //Verify if there is a match
-    for(int b = 0; b < 8; b++) {
-      if(keys[k][b]==id[b]){
-         permited++;
+    for (int b = 0; b < 8; b++) {
+      if (keys[k][b] == id[b]) {
+        permited++;
       }
     }
-    if(permited==8){
+    if (permited == 8) {
       g_armed = toggle(g_armed); // Arm/Disarm the alarm
-      permited=0;
-      for(int b = 0; b < 8; b++) {
-      id[b]=B0000000;}
-      return g_armed; 
+      permited = 0;
+      for (int b = 0; b < 8; b++) {
+        id[b] = B0000000;
+      }
+      return g_armed;
     }
-    else if(k==BD_size-1) // No match found
+    else if (k == BD_size - 1) // No match found
       return true; //Wrong key, exit_delay/1000 seconds to leave.
   }
 }
 
-boolean check_fire_button(){
+boolean check_fire_button() {
   return toggle(digitalRead(firebutton) == LOW);
 }
 
-boolean detect_fire(){
-    return (digitalRead(firesensors) == HIGH);
+boolean detect_fire() {
+  return (digitalRead(firesensors) == HIGH);
 }
 
-boolean toggle(boolean x){
-    return (!x);
+boolean isAdmin() {
+  return true;
+}
+
+boolean toggle(boolean x) {
+  return (!x);
+}
+
+boolean IsTime(unsigned long *timeMark, int timeInterval) {
+  if (millis() - *timeMark >= timeInterval) {
+    *timeMark = 0;
+    return true;
+  } else if (*timeMark == 0) {
+    *timeMark = millis();
+  }
+  return false;
 }
